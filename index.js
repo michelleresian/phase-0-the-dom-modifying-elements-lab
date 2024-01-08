0b242f02b0fda7e99207593e7a0493c0dec50aed
Michelle Resian <you@example.com>
2024-01-08 10:10:39 +0300

    DOM modifying elements labs

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const main = document.getElementById('main');
+if (main) {
+  main.remove();
+}
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const playerName = "Resian"; 
+newHeader.textContent = `${playerName} is the champion;`
+
+document.body.appendChild(newHeader);
